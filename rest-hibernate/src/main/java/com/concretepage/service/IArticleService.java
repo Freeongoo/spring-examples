e208703311994c6f8aa742a4cf4d6c331dbe72fc
dorofeev <dorofeev@actimind.com>
2018-09-14 18:28:46 +0300

    add rest hibernate

---
+++ rest-hibernate/src/main/java/com/concretepage/service/IArticleService.java
@@ -0,0 +1,13 @@
+package com.concretepage.service;
+
+import java.util.List;
+
+import com.concretepage.entity.Article;
+
+public interface IArticleService {
+     List<Article> getAllArticles();
+     Article getArticleById(int articleId);
+     boolean addArticle(Article article);
+     void updateArticle(Article article);
+     void deleteArticle(int articleId);
+}
