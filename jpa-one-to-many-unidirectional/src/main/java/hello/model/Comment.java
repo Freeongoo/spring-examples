ec75f4085046ae2a008296e1c2c847d3d2d37908
freeongoo <freeongoo@gmail.com>
2018-09-15 22:06:46 +0300

    unidir 1-to-many

---
+++ jpa-one-to-many-unidirectional/src/main/java/hello/model/Comment.java
@@ -0,0 +1,65 @@
+package hello.model;
+
+import com.fasterxml.jackson.annotation.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "comments")
+public class Comment extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Lob
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "post_id", nullable = false) // use separate field "post_id" for connect
+    // @PrimaryKeyJoinColumn for use primary key Post for connect
+    // @JoinTable(name = "comment_post",
+    //     joinColumns = @JoinColumn(name="comment_id"),
+    //     inverseJoinColumns = @JoinColumn(name="post_id")
+    // )
+    @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
+    @JsonIdentityReference(alwaysAsId=true)
+    @JsonProperty("post_id")
+    private Post post;
+
+    public Comment() {
+    }
+
+    public Comment(@NotNull String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+}
