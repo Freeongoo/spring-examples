c88bc59213108db583a7e476b2aff54844291507
freeongoo <freeongoo@gmail.com>
2018-09-17 21:11:13 +0300

    add spy for logger hibernate

---
+++ logging-hibernate/src/main/java/hello/sqlformatter/SimpleFormatter.java
@@ -0,0 +1,18 @@
+package hello.sqlformatter;
+
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
+import org.hibernate.engine.jdbc.internal.Formatter;
+
+public class SimpleFormatter implements MessageFormattingStrategy {
+    private static final Formatter HIBERNATE_SQL_FORMATTER = new BasicFormatterImpl();
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql) {
+        if (sql.isEmpty()) {
+            return "";
+        }
+        String batch = "batch".equals(category) ? " add to batch " : "";
+        return String.format("Hibernate: %s %s {elapsed: %dms}", batch, HIBERNATE_SQL_FORMATTER.format(sql), elapsed);
+    }
+}
