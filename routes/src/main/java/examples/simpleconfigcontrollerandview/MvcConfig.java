2ea06552d904d1752f325449cf90b61193f7cc46
freeongoo <freeongoo@gmail.com>
2018-09-23 17:19:42 +0300

    add mvcConfig

---
+++ routes/src/main/java/examples/simpleconfigcontrollerandview/MvcConfig.java
@@ -0,0 +1,14 @@
+package examples.simpleconfigcontrollerandview;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/simple-mvc-config").setViewName("simple-mvc-config");
+    }
+
+}
