0efa078dd4688deebbc762793c2aff8a6e110934
freeongoo <freeongoo@gmail.com>
2019-02-06 22:31:48 +0300

    crud to jpa

--- rest-jpa/src/main/java/hello/repository/oneToMany/CommentRepository.java
+++ rest-jpa/src/main/java/hello/repository/oneToMany/CommentRepository.java
@@ -1,13 +1,13 @@
 package hello.repository.oneToMany;
 
 import hello.entity.oneToMany.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     // not work, because customization Comment (add some hack fields)
     //Iterable<Comment> findAllByPostId(Long id);
