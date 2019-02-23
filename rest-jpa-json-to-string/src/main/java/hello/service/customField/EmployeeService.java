ca65e942956a3fe0e1d186de7d7acfdd1545bce4
freeongoo <freeongoo@gmail.com>
2019-02-23 16:13:38 +0300

    remove project

--- rest-jpa-json-to-string/src/main/java/hello/service/customField/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-package hello.service.customField;
-
-import hello.entity.customField.Employee;
-import hello.service.Service;
-
-public interface EmployeeService extends Service<Employee, Long> {
-
-}
