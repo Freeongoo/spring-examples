69ccbafbb7bcd086d72a90f1cb65fd4f607251a7
freeongoo <freeongoo@gmail.com>
2019-03-10 21:31:15 +0300

    delete jpaRepo

--- dao-jpa-criteria-old/src/main/java/hello/repository/oneToMany/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package hello.repository.oneToMany;
-
-import hello.entity.oneToMany.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
