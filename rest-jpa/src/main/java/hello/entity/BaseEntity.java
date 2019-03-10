c0a44dbabe79fedc51b7006ad3fd55d4891d3626
freeongoo <freeongoo@gmail.com>
2019-03-10 21:08:08 +0300

    add comment about generic base entity

--- rest-jpa/src/main/java/hello/entity/BaseEntity.java
+++ rest-jpa/src/main/java/hello/entity/BaseEntity.java
@@ -1,5 +1,11 @@
 package hello.entity;
 
+/**
+ * It is important to understand that in this solution there will be problems
+ * if you need to get the type of the primary key through reflection - return type Object
+ *
+ * @param <ID> Primary key
+ */
 public interface BaseEntity<ID> {
 
     ID getId();
