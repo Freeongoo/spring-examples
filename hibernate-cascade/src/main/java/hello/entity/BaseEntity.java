20c63c0e13be09d3106cc2072010a6008ff3126b
freeongoo <freeongoo@gmail.com>
2019-04-08 23:27:58 +0300

    add java docs

--- hibernate-cascade/src/main/java/hello/entity/BaseEntity.java
+++ hibernate-cascade/src/main/java/hello/entity/BaseEntity.java
@@ -1,8 +1,18 @@
 package hello.entity;
 
+/**
+ * not the best idea to do generic <ID> for primary key
+ * since when working with an entity through reflection, it will receive a type as an {@link Object}
+ *
+ * @param <ID> primary key
+ */
 public interface BaseEntity<ID> {
+
     ID getId();
+
     void setId(ID id);
+
     String getName();
+
     void setName(String name);
 }
--- hibernate-many-to-many/src/main/java/hello/entity/BaseEntity.java
+++ hibernate-many-to-many/src/main/java/hello/entity/BaseEntity.java
@@ -1,5 +1,11 @@
 package hello.entity;
 
+/**
+ * not the best idea to do generic <ID> for primary key
+ * since when working with an entity through reflection, it will receive a type as an {@link Object}
+ *
+ * @param <ID> primary key
+ */
 public interface BaseEntity<ID> {
 
     ID getId();
