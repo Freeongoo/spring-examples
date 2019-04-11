9bf610901fa66f8fc1e12ebd116aebe6990465fe
freeongoo <freeongoo@gmail.com>
2019-04-11 23:16:54 +0300

    add test orphanRemove = false

--- hibernate-orphan-removal/src/test/java/hello/entity/orphanRemoval/PersonTest.java
+++ hibernate-orphan-removal/src/test/java/hello/entity/orphanRemoval/PersonTest.java
@@ -3,7 +3,12 @@ package hello.entity.orphanRemoval;
 import hello.AbstractJpaTest;
 import org.junit.Test;
 
+import javax.persistence.PersistenceException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class PersonTest extends AbstractJpaTest {
@@ -27,5 +32,70 @@ public class PersonTest extends AbstractJpaTest {
         Phone phoneFromDb = entityManager.find(Phone.class, phone1.getId());
         assertThat(phoneFromDb, equalTo(null));   // removed because orphanRemoval = true
                                                           // (if orphanRemoval = false) only remove relation
+    }
+
+    @Test
+    public void removeOneFromList_WhenRemoveOnlyFromAuthorSide_ShouldBeDeleted() {
+        Person person = new Person();
+        Phone phone1 = new Phone("123-456-7890");
+        Phone phone2 = new Phone("321-654-0987");
+
+        person.addPhone(phone1);
+        person.addPhone(phone2);
+
+        entityManager.persist(person);
+        entityManager.flush();
+
+        person.getPhones().remove(phone1);
+
+        flushAndClean();
+
+        Phone phoneFromDb = entityManager.find(Phone.class, phone1.getId());
+        assertThat(phoneFromDb, equalTo(null));
+
+        Person personFromDb = entityManager.find(Person.class, person.getId());
+        assertThat(personFromDb.getPhones().size(), equalTo(1));
+    }
+
+    @Test
+    public void remove_WhenRemoveAllPhones_ShouldBeDeleted() {
+        Person person = new Person();
+        Phone phone1 = new Phone("123-456-7890");
+        Phone phone2 = new Phone("321-654-0987");
+
+        person.addPhone(phone1);
+        person.addPhone(phone2);
+
+        entityManager.persist(person);
+        entityManager.flush();
+
+        person.getPhones().clear();
+
+        flushAndClean();
+
+        Phone phoneFromDb = entityManager.find(Phone.class, phone1.getId());
+        assertThat(phoneFromDb, equalTo(null));
+        Phone phoneFromDb2 = entityManager.find(Phone.class, phone2.getId());
+        assertThat(phoneFromDb2, equalTo(null));
+
+        Person personFromDb = entityManager.find(Person.class, person.getId());
+        assertThat(personFromDb.getPhones().size(), equalTo(0));
+    }
+
+    @Test(expected = PersistenceException.class)
+    public void remove_WhenWrongRemoveAllPhones_ShouldBeThrowException() {
+        Person person = new Person();
+        Phone phone1 = new Phone("123-456-7890");
+        Phone phone2 = new Phone("321-654-0987");
+
+        person.addPhone(phone1);
+        person.addPhone(phone2);
+
+        entityManager.persist(person);
+        entityManager.flush();
+
+        person.setPhones(null); // don't do this --> use: person.getPhones().clear();
+
+        flushAndClean();
     }
 }
--- hibernate-orphan-removal/src/test/java/hello/entity/withoutOrphanRemoval/AuthorTest.java
+++ hibernate-orphan-removal/src/test/java/hello/entity/withoutOrphanRemoval/AuthorTest.java
@@ -3,8 +3,11 @@ package hello.entity.withoutOrphanRemoval;
 import hello.AbstractJpaTest;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -28,6 +31,77 @@ public class AuthorTest extends AbstractJpaTest {
 
         Book bookFromDb = entityManager.find(Book.class, book1.getId());
         assertThat(bookFromDb, is(notNullValue()));
-        assertThat(bookFromDb.getAuthor(), nullValue());
+        assertThat(bookFromDb.getAuthor(), nullValue()); // nothing deleted
+    }
+
+    @Test
+    public void removeOneFromList_WhenRemoveOnlyFromAuthorSide_ShouldBeNotDeleted() {
+        Author author = new Author();
+        Book book1 = new Book("123-456-7890");
+        Book book2 = new Book("321-654-0987");
+
+        author.addBook(book1);
+        author.addBook(book2);
+
+        entityManager.persist(author);
+        entityManager.flush();
+
+        // try delete only from author side
+        List<Book> books = author.getBooks();
+        books.remove(book1);
+
+        flushAndClean();
+
+        Book bookFromDb = entityManager.find(Book.class, book1.getId());
+        assertThat(bookFromDb, is(notNullValue()));
+        assertThat(bookFromDb.getAuthor(), is(notNullValue())); // nothing deleted
+    }
+
+    @Test
+    public void remove_WhenRemoveAllBooks_ShouldBeNotDeleted() {
+        Author author = new Author();
+        Book book1 = new Book("123-456-7890");
+        Book book2 = new Book("321-654-0987");
+
+        author.addBook(book1);
+        author.addBook(book2);
+
+        entityManager.persist(author);
+        entityManager.flush();
+
+        author.getBooks().clear();
+
+        flushAndClean();
+
+        Book bookFromDb = entityManager.find(Book.class, book1.getId());
+        assertThat(bookFromDb, is(notNullValue()));
+        assertThat(bookFromDb.getAuthor(), is(notNullValue())); // nothing deleted
+
+        Author authorFromDb = entityManager.find(Author.class, author.getId());
+        assertThat(authorFromDb.getBooks().size(), equalTo(2)); // nothing deleted
+    }
+
+    @Test
+    public void remove_WhenWrongRemoveAllBooks_ShouldBeNotDeleted() {
+        Author author = new Author();
+        Book book1 = new Book("123-456-7890");
+        Book book2 = new Book("321-654-0987");
+
+        author.addBook(book1);
+        author.addBook(book2);
+
+        entityManager.persist(author);
+        entityManager.flush();
+
+        author.setBooks(null);
+
+        flushAndClean();
+
+        Book bookFromDb = entityManager.find(Book.class, book1.getId());
+        assertThat(bookFromDb, is(notNullValue()));
+        assertThat(bookFromDb.getAuthor(), is(notNullValue())); // nothing deleted
+
+        Author authorFromDb = entityManager.find(Author.class, author.getId());
+        assertThat(authorFromDb.getBooks().size(), equalTo(2)); // nothing deleted
     }
 }
