e9664799c55f4a04f969d81367fcb9722548d938
freeongoo <freeongoo@gmail.com>
2019-10-30 00:17:32 +0300

    simple web flux

---
+++ web-flux-rest-simple/readme.md
@@ -0,0 +1,3 @@
+# Building a Reactive RESTful Web Service
+
+https://spring.io/guides/gs/reactive-rest-service/
---
+++ web-flux-rest-simple/src/main/java/com/example/demo/GreetingHandler.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GreetingHandler {
+    public Mono<ServerResponse> hello(ServerRequest request) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(BodyInserters.fromValue("Hello, Spring!"));
+    }
+}
